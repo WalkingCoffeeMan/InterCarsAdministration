899f0850d7bbeaae4454e378fc01ed0e5cd6dad7
WalkingCoffeeMan <onur.patlak@hotmail.com>
2024-08-04 21:43:20 +0200

    Initial project

---
+++ src/main/java/com/backend/workshopdatabackend/WorkshopDataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.workshopdatabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDataBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopDataBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/workshopdatabackend/WorkshopDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.workshopdatabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopDataBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
